11b3e153b26dfbae9d29ce856871e7fe66b94ba0
Fleege <76089935+pfleege@users.noreply.github.com>
2025-01-10 17:26:06 +0200

    ADD about page + styling to navbar links

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,11 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex flex-col justify-between h-screen py-[5vw] bg-center bg-cover bg-[url('/navBg.jpg')]">
       <div className="flex items-start">
@@ -29,18 +33,54 @@ export default function Navbar() {
           </p>
         </div>
       </div>
-      <ul className="flex w-full justify-evenly text-4xl font-rubicGlitch text-white">
+      <ul className="flex h-[100px] w-full items-center justify-evenly text-4xl font-rubicGlitch bg-violet-900 bg-opacity-50">
         <li>
-          <Link href="/">Home</Link>
+          <Link
+            href="/"
+            className={
+              pathname === "/"
+                ? "text-blue-600 py-1 px-3 border-y-4 border-blue-700"
+                : "text-neutral-400 hover:text-orange-400"
+            }
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/client">About</Link>
+          <Link
+            href="/about"
+            className={
+              pathname === "/about"
+                ? "text-blue-400 py-1 px-3 border-y-4 border-blue-700"
+                : "text-neutral-400 hover:text-orange-400"
+            }
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link href="/api/auth/signin">Sign In</Link>
+          <Link
+            href="/api/auth/signin"
+            className={
+              pathname === "/api/auth/signin"
+                ? "text-blue-600 py-1 px-3 border-y-4 border-blue-700"
+                : "text-neutral-400 hover:text-orange-400"
+            }
+          >
+            Sign In
+          </Link>
         </li>
         <li>
-          <Link href="/api/auth/signout">Sign Out</Link>
+          <Link
+            href="/api/auth/signout"
+            className={
+              pathname === "/api/auth/signout"
+                ? "text-blue-600 py-1 px-3 border-y-4 border-blue-700"
+                : "text-neutral-400 hover:text-orange-400"
+            }
+          >
+            Sign Out
+          </Link>
         </li>
       </ul>
     </nav>
