f26619520e5a39d6413ed65c54ecb271f4e1ab66
Fleege <76089935+pfleege@users.noreply.github.com>
2025-01-10 19:58:23 +0200

    ADD login options CredentialsProvider and OAuth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,39 @@
 import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
 
 export const options: NextAuthOptions = {
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Demo username = 'testuser'",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Demo password = 'nextauth'",
+        },
+      },
+      async authorize(credentials) {
+        // Hard-coded test-user credentials -> use DB in production-ready app
+        const user = { id: "1", name: "testuser", password: "nextauth" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        }
+        return null;
+      },
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
 };
