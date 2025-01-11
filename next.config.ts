d34b4780568db325d81991c6cd110a2c74c3a13c
Fleege <76089935+pfleege@users.noreply.github.com>
2025-01-11 18:45:54 +0200

    ADD image configurations to next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 };
-
 export default nextConfig;
