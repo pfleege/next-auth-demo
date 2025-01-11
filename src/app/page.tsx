c7dd4c1160b2bb732d9ca1fe972bf610613304dc
Fleege <76089935+pfleege@users.noreply.github.com>
2025-01-11 18:44:01 +0200

    ADD UserCard component + update Home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-export default function Home() {
+import { options } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
+import UserCard from "./components/UserCard";
+
+export default async function Home() {
+  const session = await getServerSession(options);
   return (
-    <div className="font-specialElite text-4xl text-center">
-      <h2>Home</h2>
-      <p className="font-raleway text-base italic">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quia
-        ducimus illo eum neque voluptas error architecto libero, facere tempore!
-      </p>
+    <div className="flex flex-col items-center font-specialElite text-4xl text-center bg-gradient-to-b from-indigo-900 from-10% via-sky-500 via-30% to-fuchsia-950 to-90%">
+      {session ? (
+        <div className="flex flex-col items-center my-20 p-20 w-[60vw] rounded-xl shadow-lg bg-slate-900 border-4 border-fuchsia-700">
+          <UserCard user={session?.user} />
+        </div>
+      ) : (
+        <h1 className="text-5xl text-white">
+          Please log in to view more content.
+        </h1>
+      )}
     </div>
   );
 }
