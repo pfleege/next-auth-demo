3c36855690708aae25eff9582756cab2442f54f3
Fleege <76089935+pfleege@users.noreply.github.com>
2025-01-12 16:30:28 +0200

    MOD About page background and add Footnote z-index

--- src/app/components/Footnote.tsx
+++ src/app/components/Footnote.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const Footnote = () => {
   return (
-    <div className="flex justify-end items-center gap-1 fixed bottom-2 right-0 bg-neutral-500 bg-opacity-50 rounded-l-2xl">
-      <p className="text-white text-[14px] py-1 pl-5">NextAuth demo by:</p>
+    <div className="flex justify-end items-center gap-1 fixed bottom-2 right-0 bg-neutral-500 bg-opacity-50 rounded-l-2xl z-50">
+      <p className="text-white text-[14px] pt-1 pl-5">NextAuth demo by:</p>
       <Link href="https://iceeqsolutions.fi" target="_blank">
         <Image
           src={"/iCeeqSolutionsLogo.png"}
